17bf171bd0ce94153c07793524429082d299134c
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-03-01 15:17:11 +0000

    third

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,10 @@ class ListsController < ApplicationController
   def index
     @lists = List.all
   end
-  
+
+  def show
+    @list =List.find(params[:id])
+  end
 
   def create
     list = List.new(list_params)
@@ -25,8 +28,7 @@ class ListsController < ApplicationController
   end
 
   private
-  
-  def list_params
-    params.require(:list).permit(:title,:body)
+ def list_params
+    params.require(:list).permit(:title, :body, :image)
   end
 end
