cfa216c084f3dfd4632464cd603553e6fa962622
ikedoasako <124412564+ikedoasako@users.noreply.github.com>
2023-03-04 08:43:20 +0000

    commit4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,13 +12,22 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :index #<=newからindexに変更
+    end
   end
 
   def edit
     @list = List.find(params[:id])
+  end
+  
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
   end
 
   def update
@@ -30,5 +39,5 @@ class ListsController < ApplicationController
   private
  def list_params
     params.require(:list).permit(:title, :body, :image)
-  end
+ end
 end
